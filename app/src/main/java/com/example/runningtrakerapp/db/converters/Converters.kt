008f01a33ff5b11397961ae5972dbdd30000b271
Mian Muzammil <107386284+MianMuzammil67@users.noreply.github.com>
2025-01-12 22:00:27 +0500

    Update Converters.kt

--- app/src/main/java/com/example/runningtrakerapp/db/converters/Converters.kt
+++ app/src/main/java/com/example/runningtrakerapp/db/converters/Converters.kt
@@ -7,19 +7,6 @@ import java.io.ByteArrayOutputStream
 
 
 class Converters {
-
-//    @TypeConverter
-//    fun toBitmap(bytes: ByteArray): Bitmap {
-//        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-//    }
-//
-//    @TypeConverter
-//    fun fromBitmap(bmp: Bitmap): ByteArray {
-//        val outputStream = ByteArrayOutputStream()
-//        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
-//        return outputStream.toByteArray()
-//    }
-
     @TypeConverter
     fun toBitmap(bytes: ByteArray?): Bitmap? {
         // Handle null byte arrays
